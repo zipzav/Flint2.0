5ca78cfcee32faa8fceaf013b52ea9d2476ea2d8
Zavier Aguila <zavieraguila@gmail.com>
2017-06-10 10:47:02 -0700

    Initial Commit

---
+++ Flint2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flint2.0
+//
+//  Created by Zavier Patrick David Aguila on 6/10/17.
+//  Copyright © 2017 Zavier Patrick David Aguila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Flint2.0Tests/Flint2_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Flint2_0Tests.swift
+//  Flint2.0Tests
+//
+//  Created by Zavier Patrick David Aguila on 6/10/17.
+//  Copyright © 2017 Zavier Patrick David Aguila. All rights reserved.
+//
+
+import XCTest
+@testable import Flint2_0
+
+class Flint2_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
